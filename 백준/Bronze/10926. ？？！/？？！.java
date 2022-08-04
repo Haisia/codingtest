cfc7a8775b01d1f8f6ddccbf987c36a3fc8a9f67
Haisia <110602188+Haisia@users.noreply.github.com>
2022-08-04 21:57:59 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String A;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.next();
+		
+		System.out.println(A+"??!");
+	}
+
+}
