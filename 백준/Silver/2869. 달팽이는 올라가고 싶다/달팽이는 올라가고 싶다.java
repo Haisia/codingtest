b08657677d9bd831514e8e04eb98a3bcd1d1997d
Haisia <110602188+Haisia@users.noreply.github.com>
2022-08-06 16:45:59 +0900

    [Silver V] Title: 달팽이는 올라가고 싶다, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		double a=Integer.parseInt(st.nextToken());
+		double b=Integer.parseInt(st.nextToken());
+		double v=Integer.parseInt(st.nextToken());
+		int day;
+		
+		
+		
+		if((v-a)%(a-b)>b) {
+			day=(int) (Math.ceil((v-a)/(a-b)))+1;
+		}else {
+//			System.out.println((v-a)/(a-b));
+//			System.out.println(Math.ceil((v-a)/(a-b)));
+			day=(int) (Math.ceil((v-a)/(a-b)))+1;
+		}
+		
+		
+		
+		bw.write(Integer.toString(day));
+		bw.close();
+	}
+}
+
