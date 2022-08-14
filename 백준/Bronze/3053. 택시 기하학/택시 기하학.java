2e14d7dac42e4c6276e0cac86cb59727567f76ab
Haisia <110602188+Haisia@users.noreply.github.com>
2022-08-15 01:32:20 +0900

    [Bronze III] Title: 택시 기하학, Time: 216 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	int r = sc.nextInt();
+    	
+    	System.out.println(Math.pow(r, 2)*Math.PI);
+    	System.out.println(Math.pow(r, 2)*2);
+    	
+    	
+    }
+}
