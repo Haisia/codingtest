6cca57ab2d13d7c37ae9d60ef4f636d6c19568e0
Haisia <110602188+Haisia@users.noreply.github.com>
2022-08-27 10:42:29 +0900

    [Bronze I] Title: 최소공배수, Time: 368 ms, Memory: 22540 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i=0; i<t; i++) {
+            int[] arr = new int[2];
+
+            arr[0] = sc.nextInt();
+            arr[1] = sc.nextInt();
+            Arrays.sort(arr);
+
+            int gcf = tool(arr[0], arr[1]);
+            System.out.println((arr[0] / gcf) * (arr[1] / gcf) * gcf);
+        }
+    }
+
+    static int tool(int a, int b){
+        int c;
+
+        while(a!=0) {
+            c = b % a;
+            b = a;
+            a = c;
+        }
+        return b;
+    }
+}
