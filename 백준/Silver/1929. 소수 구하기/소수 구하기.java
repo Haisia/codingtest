4ae1288ac5e06403a9fad2320534c9ab26134cef
Haisia <110602188+Haisia@users.noreply.github.com>
2022-09-17 12:07:19 +0900

    [Silver III] Title: 소수 구하기, Time: 928 ms, Memory: 34800 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
+/**
+ * 1. m과 n을 입력받는다.
+ * 2. '에라토스테네스의 체' 를 사용하여 소수를 찾는다.
+ *      - 범위 내의 모든 수의 상태를 'true' 로 놓고, 소수가 아닌 수를 찾아서 false 로 변경한다.
+ *      - 소수가 아닌 모든 수는 (i^2 + k*i) 로 표현 가능 (k는 음수가 아닌 정수)
+ *      - 위의 식에 해당하는 수의 상태를 'false' 로 변경
+ * 3. m이상, n이하를 만족하는 소수를 출력한다.
+ */
+
 public class Main {
     public static void main(String[] args) {
         // 1. 입력
